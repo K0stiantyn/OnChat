ef4bf6a1310c4a114ad37c84e5cfed5277c9eacd
K0STI@ <ksinogub333@gmail.com>
2024-04-29 15:07:53 +0200

    f

--- OnlineChat.py
+++ OnlineChat.py
@@ -14,25 +14,25 @@ MAX_MESSAGES_COUNT = 100
 async def main():
     global chat_msgs
 
-    put_markdown("## 🧊 Добро пожаловать в онлайн чат!\n")
+    put_markdown("## 💬 Вітаємо у чаті!\n")
 
     msg_box = output()
     put_scrollable(msg_box, height=300, keep_bottom=True)
 
-    nickname = await input("Войти в чат", required=True, placeholder="Ваше имя",
-                           validate=lambda n: "Такой ник уже используется!" if n in online_users or n == '📢' else None)
+    nickname = await input("Ввійти в чат", required=True, placeholder="Ім'я",
+                           validate=lambda n: "Такий нік вже використовується!" if n in online_users or n == '📢' else None)
     online_users.add(nickname)
 
-    chat_msgs.append(('📢', f'`{nickname}` присоединился к чату!'))
-    msg_box.append(put_markdown(f'📢 `{nickname}` присоединился к чату'))
+    chat_msgs.append(('📢', f'`{nickname}` доєднався до чату!'))
+    msg_box.append(put_markdown(f'📢 `{nickname}` доєднався до чату'))
 
     refresh_task = run_async(refresh_msg(nickname, msg_box))
 
     while True:
-        data = await input_group("💭 Новое сообщение", [
-            input(placeholder="Текст сообщения ...", name="msg"),
-            actions(name="cmd", buttons=["Отправить", {'label': "Выйти из чата", 'type': 'cancel'}])
-        ], validate=lambda m: ('msg', "Введите текст сообщения!") if m["cmd"] == "Отправить" and not m['msg'] else None)
+        data = await input_group("💭 Нове повідомлення", [
+            input(placeholder="Текст повідомлення ...", name="msg"),
+            actions(name="cmd", buttons=["Відправити", {'label': "Вийти з чату", 'type': 'cancel'}])
+        ], validate=lambda m: ('msg', "Введіть текст повідомлення!") if m["cmd"] == "Відправити" and not m['msg'] else None)
 
         if data is None:
             break
@@ -43,9 +43,9 @@ async def main():
     refresh_task.close()
 
     online_users.remove(nickname)
-    toast("Вы вышли из чата!")
-    msg_box.append(put_markdown(f'📢 Пользователь `{nickname}` покинул чат!'))
-    chat_msgs.append(('📢', f'Пользователь `{nickname}` покинул чат!'))
+    toast("Ви вийшли з чату!")
+    msg_box.append(put_markdown(f'📢 Користувач `{nickname}` покинув чат!'))
+    chat_msgs.append(('📢', f'Користувач `{nickname}` покинув чат!'))
 
     put_buttons(['Перезайти'], onclick=lambda btn: run_js('window.location.reload()'))
 
@@ -58,7 +58,7 @@ async def refresh_msg(nickname, msg_box):
         await asyncio.sleep(1)
 
         for m in chat_msgs[last_idx:]:
-            if m[0] != nickname:  # if not a message from current user
+            if m[0] != nickname:
                 msg_box.append(put_markdown(f"`{m[0]}`: {m[1]}"))
 
         # remove expired
